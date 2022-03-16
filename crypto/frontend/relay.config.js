9926cd80ff51f89597563d988a9088f05b5095ea
Martin Artola <martin.artola@swisslife.ch>
2022-03-16 10:20:48 +0100

    initial frontend

---
+++ crypto/frontend/relay.config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  schema: 'schema/server.graphql',
+  schemaExtensions: ['schema'],
+  src: 'scenes',
+  customScalars: {
+    Date: 'String',
+    DateTime: 'String',
+    Upload: 'null',
+    Uuid: 'String',
+  },
+  eagerEsModules: true,
+  noFutureProofEnums: false,
+  language: 'flow',
+
+  // @see https://relay.dev/docs/guides/type-emission/#single-artifact-directory
+  artifactDirectory: 'generated',
+
+  // @see https://relay.dev/docs/guides/persisted-queries/
+  // persistConfig: {
+  //   // Remote Persisting
+  //   url: 'http://localhost:4000/persist',
+  //   params: {},
+  //   concurrency: 10,
+  //
+  //   // Local Persisting
+  //   file: './persisted.json',
+  // },
+};
