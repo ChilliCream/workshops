b39174497063dce025740b8cbb777dfc85cdd9d9
Martin Artola <martin.artola@swisslife.ch>
2022-04-21 12:41:21 +0200

    change to favorites

--- crypto/frontend/icons/ethereum-classic-icon.js
+++ crypto/frontend/icons/ethereum-classic-icon.js
@@ -4,7 +4,7 @@ export default function EthereumIcon(props) {
   return (
     <SvgIcon viewBox="0 0 32 32" {...props}>
       <g>
-        <circle cx="16" cy="16" r="16" fill="#627eea" />
+        <circle cx="16" cy="16" r="16" fill="#12de8d" />
         <g fill="#fff">
           <path fillOpacity=".6" d="M16.5 4v8.9l7.5 3.3z" />
           <path d="M16.5 4 9 16.2l7.5-3.3z" />
