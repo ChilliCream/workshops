e6bd71b8b2ff586bc61664af7e21c24862241cd7
Martin Artola <martin@artola.info>
2022-07-11 14:29:44 +0200

    improved components

--- crypto/frontend/components/no-data.js
+++ crypto/frontend/components/no-data.js
@@ -1,12 +1,19 @@
 import {Typography} from '@mui/material';
+import {forwardRef} from 'react';
 
-export const NoData = ({message = 'No data available.', ...rest}) => (
-  <Typography
-    component="div"
-    variant="body2"
-    align="center"
-    p={4}
-    children={message}
-    {...rest}
-  />
-);
+export const NoData = forwardRef(function NoDate(
+  {message = 'No data available.', ...rest},
+  ref,
+) {
+  return (
+    <Typography
+      ref={ref}
+      component="div"
+      variant="body2"
+      align="center"
+      p={4}
+      children={message}
+      {...rest}
+    />
+  );
+});
--- crypto/frontend/components/transition-indicator.js
+++ crypto/frontend/components/transition-indicator.js
@@ -1,13 +1,20 @@
 import {Fade} from '@mui/material';
 
-export const TransitionIndicator = ({in: inProp = false, ...rest}) => (
+export const TransitionIndicator = (props) => (
   <Fade
-    in={inProp}
     style={{
-      filter: inProp && 'blur(2px)',
-      opacity: 1,
-      visibility: 'visible',
+      opacity: '',
+      visibility: '',
     }}
-    {...rest}
+    onEnter={(node) => {
+      node.style.transitionProperty = 'filter';
+      node.style.transitionDelay = '300ms';
+      node.style.filter = 'blur(2px)';
+    }}
+    onExit={(node) => {
+      node.style.transition = '';
+      node.style.filter = '';
+    }}
+    {...props}
   />
 );
