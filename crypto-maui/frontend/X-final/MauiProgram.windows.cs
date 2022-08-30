d2a827112bbba463f194fd533ab36bcf222f4824
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-08-29 19:18:38 -0700

    Create MauiProgram.windows.cs

---
+++ crypto-maui/frontend/X-final/MauiProgram.windows.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiCrypto;
+
+public static partial class MauiProgram
+{
+	static MauiProgram()
+	{
+#if DEBUG
+		_apiUrl = "https://localhost:51582/graphql/";
+#else
+#error GraphQL API Not Defined
+		_apiUrl = "";
+#endif
+	}
+
+	static HttpMessageHandler GetHttpMessageHandler() => new HttpClientHandler { AutomaticDecompression = GetDecompressionMethods() };
+}
+
