91109ecea27864e46c90118e7b1724b9180c3f13
Michael Staib <michael@chillicream.com>
2022-09-24 15:54:22 +0200

    Reworked title

--- crypto-maui/docs/src/pages/index.js
+++ crypto-maui/docs/src/pages/index.js
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">crypto - {siteConfig.title}</h1>
+        <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description={siteConfig.tagline}
     >
       <HomepageHeader />
