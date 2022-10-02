5b3822117c58049e1e6d2fcfcfe7b72c284a9d4b
Martin Artola <martin@artola.info>
2022-10-02 02:35:08 +0200

    fix redirect

--- crypto/frontend/playground/4-viewer/pages/index.js
+++ crypto/frontend/playground/4-viewer/pages/index.js
@@ -1,5 +1,7 @@
 import {Redirect} from '@/components';
 
 export default function Viewer() {
-  return <Redirect url={{pathname: '/currencies/BTC'}} />;
+  return (
+    <Redirect url={{pathname: '/currencies/[symbol]'}} as="/currencies/BTC" />
+  );
 }
