09322005e2fbd14d8e7865e6f1cc0e03feff87f4
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-11-10 13:11:16 -0800

    Fix `AssetChartPage.Padding`

--- crypto-maui/frontend/X-final/MauiProgram.cs
+++ crypto-maui/frontend/X-final/MauiProgram.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http.Headers;
-using System.Runtime.CompilerServices;
 using System.Text;
 using CommunityToolkit.Maui;
 using CommunityToolkit.Maui.Markup;
