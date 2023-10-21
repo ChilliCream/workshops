52fc006a8566a40a2aed9c9320b921f9d6038ea0
Martin Artola <martin@chillicream.com>
2023-10-21 19:19:17 +0200

    bump deps for crypto-rn

---
+++ crypto-rn/frontend/src/components/core/avatar.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/native';
+import React from 'react';
+import {Image, View} from 'react-native';
+import type {ImageProps} from 'react-native';
+
+export type AvatarSize = 'small' | 'medium' | 'large';
+
+export interface AvatarProps {
+  size: AvatarSize;
+  src?: ImageProps['source'] | string;
+}
+
+const getSize = (size: AvatarSize): number =>
+  ({
+    small: 20,
+    medium: 32,
+    large: 40,
+  })[size];
+
+const Root = styled(View)<{size: AvatarSize}>`
+  ${({size}) => `
+    width: ${getSize(size)}px;
+    height: ${getSize(size)}px;
+    border-radius: ${getSize(size) / 2}px;
+  `}
+`;
+
+export const Avatar: React.FC<React.PropsWithChildren<AvatarProps>> = ({
+  size,
+  src,
+  children,
+}) => {
+  const source =
+    typeof src === 'string' ? {uri: `https:${src.split(':')[1]}`} : src;
+
+  return (
+    <Root size={size}>
+      {src ? (
+        <Image
+          style={{width: getSize(size), height: getSize(size)}}
+          source={source}
+        />
+      ) : (
+        children
+      )}
+    </Root>
+  );
+};
