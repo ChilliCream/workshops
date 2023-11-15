ec96806ac0dfdf06fd9a8e254e044a8a99d99dfb
Martin Artola <martin@chillicream.com>
2023-11-15 12:27:19 +0100

    improve prettier config

--- crypto/frontend/.proactive/prettier-config/index.js
+++ crypto/frontend/.proactive/prettier-config/index.js
@@ -15,12 +15,12 @@ module.exports = {
   importOrder: ['', '', '^@/(.*)$', '', '^[./]'],
   importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
   importOrderTypeScriptVersion: '5.0.0',
-  "overrides": [
+  overrides: [
     {
-      "files": ["*.yml", "*.yaml"],
-      "options": {
-        "singleQuote": false
-      }
-    }
-  ]
+      files: ['*.yml', '*.yaml'],
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
